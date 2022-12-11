c4ae37ea64f76a869aaacf8e5947c15831be46fc
Advait Alluri <advait.alluri@gmail.com>
2022-12-10 22:10:18 -0800

    Added Comment

--- LeetCode/rotate_image.py
+++ LeetCode/rotate_image.py
@@ -1,3 +1,5 @@
+#This function will rotate a 2D list clockwise as if it is an image
+
 def rotate(matrix):
     copy = matrix.copy()
     copy.reverse()
@@ -6,14 +8,9 @@ def rotate(matrix):
         new = []
         for k in range(length):
             new.append(copy[k][i])
-
-       # import pdb
-        #pdb.set_trace()
         matrix[i] = new
 
-    return(matrix)
-
-
+    return matrix
 
 
 print(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
